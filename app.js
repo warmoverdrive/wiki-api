0ff902da65aecf69a19221f5e8d0fd21f15b1482
warmoverdrive <sweetsthebear@gmail.com>
2021-04-03 10:27:23 -0700

    Refactor /articles route into .route chain. comments for visual seperation of code

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//==============================================================//
+// Setup
+//==============================================================//
+
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
@@ -6,6 +10,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//==============================================================//
+// MongoDB Setup
+//==============================================================//
 
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {
   useNewUrlParser: true,
@@ -20,41 +28,49 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", (req, res) => {
-  Article.find({}, (err, articles) => {
-    if (!err) {
-      res.send(articles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+//==============================================================//
+// '/articles' route handling
+//==============================================================//
 
-app.post("/articles", (req, res) => {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
+app
+  .route("/articles")
+  .get((req, res) => {
+    Article.find({}, (err, articles) => {
+      if (!err) {
+        res.send(articles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save((err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  })
+  .delete((req, res) => {
+    Article.deleteMany({}, (err, articles) => {
+      if (err) {
+        res.send(err);
+      } else {
+        console.log("Successfully deleted all articles");
+        res.sendStatus(200);
+      }
+    });
   });
 
-  newArticle.save((err) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-});
-
-app.delete("/articles", (req, res) => {
-  Article.deleteMany({}, (err, articles) => {
-    if (err) {
-      res.send(err);
-    } else {
-      console.log("Successfully deleted all articles");
-      res.sendStatus(200);
-    }
-  });
-});
+//==============================================================//
+// Server start
+//==============================================================//
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
